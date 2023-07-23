14939bc8b0a4141ed5d0ab56e4a318c53d776a6c
Sergio Donato <sergiodcdn@gmail.com>
2023-07-23 18:06:10 -0300

    mongoDB atlas conected

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CRUD_API_NODE_EXPRESS_MONGODB
   Simple CRUD API with Node.js, Express and MongoDB from scratch.
+  Use Insomia to test API through, and MongoDB Atlas if database
 
 npm init -y ( build package.json )
 
@@ -11,3 +12,20 @@ npm init -y ( build package.json )
 npm run serve ( to run server.js )
 
 npm i express
+
+npm i nodemon -D
+
+*add in package.json
+    "scripts": {
+      "dev": "nodemon server.js"
+  }
+
+npm run dev ( instant modify to app )
+
+npm i mongoose
+
+MONGODB_URI = mongodb+srv://<user>:<password>@cluster0.hduktsv.mongodb.net/?retryWrites=true&w=majority
+SECRET_JWT = 
+
+MONGODB_URI = mongodb+srv://root69:root69@cluster0.hduktsv.mongodb.net/?retryWrites=true&w=majority
+SECRET_JWT = 3113412251f19d929abfbc139874ed44
--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
 
 // -- routes
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/te", (req, res) => {
+  res.send("Hello Teteco!");
+});
 
-app.listen(port, () => {
-  console.log(`NODE API is listening on port ${port}`)
-})
+mongoose
+  .connect(
+    "mongodb+srv://root69:root69@cluster0.hduktsv.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to Mongo DB");
+    app.listen(port, () => {
+      console.log(`NODE API is listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
